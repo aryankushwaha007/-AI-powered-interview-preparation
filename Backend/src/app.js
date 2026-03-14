dcf01af7de9a2b97ec4dfbcb630e16e958f8571f
aryankushwaha007 <aryankushwaha279262@gmail.com>
2026-03-15 01:10:07 +0530

    cors updated for vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://ai-powered-interview-preparation.vercel.app"
+    ],
     credentials: true
 }))
+
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
